638ade0eeb9cf0dc15261220a210d0d40f05da41
NAnuradha15 <panduwawalaa98@gmail.com>
2024-03-19 22:11:34 +0530

    Initial Commit

---
+++ testing11/testing11App.swift
@@ -0,0 +1,17 @@
+//
+//  testing11App.swift
+//  testing11
+//
+//  Created by User on 2024-03-19.
+//
+
+import SwiftUI
+
+@main
+struct testing11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
